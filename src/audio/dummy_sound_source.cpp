3696d688019c0a6bb93d3f44ce15733b8df20104
DeltaResero <3397617+DeltaResero@users.noreply.github.com>
2025-11-18 23:04:51 -0400

    Wire up SDL_mixer backend and fix Dummy audio source
    This integrates the new audio backend into the build system and
    fixes an initialization bug.
    
    configure.ac:
    - Added AC_ARG_ENABLE for 'openal'
    - If --disable-openal is passed, the build system now searches for
      SDL_mixer >= 1.2 instead and defines USE_SDL_MIXER
    
    DummySoundSource:
    - Initialized 'is_playing' to false in the constructor
      (prevents undefined behavior)

--- src/audio/dummy_sound_source.cpp
+++ src/audio/dummy_sound_source.cpp
@@ -1,4 +1,4 @@
-//  $Id$
+//  src/audio/dummy_sound_source.cpp
 //
 //  SuperTux
 //  Copyright (C) 2006 Matthias Braun <matze@braunis.de>
@@ -16,6 +16,7 @@
 //  You should have received a copy of the GNU General Public License
 //  along with this program; if not, write to the Free Software
 //  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
 #include <config.h>
 
 #include "dummy_sound_source.hpp"
@@ -24,7 +25,9 @@ class DummySoundSource : public SoundSource
 {
 public:
   DummySoundSource()
+    : is_playing(false)
   {}
+
   virtual ~DummySoundSource()
   {}
 
@@ -79,3 +82,5 @@ SoundSource* create_dummy_sound_source()
 {
   return new DummySoundSource();
 }
+
+// EOF
--- src/audio/dummy_sound_source.hpp
+++ src/audio/dummy_sound_source.hpp
@@ -1,4 +1,4 @@
-//  $Id$
+//  src/audio/dummy_sound_source.hpp
 //
 //  SuperTux
 //  Copyright (C) 2006 Matthias Braun <matze@braunis.de>
@@ -16,6 +16,7 @@
 //  You should have received a copy of the GNU General Public License
 //  along with this program; if not, write to the Free Software
 //  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
 #ifndef __DUMMY_SOUND_SOURCE_HPP__
 #define __DUMMY_SOUND_SOURCE_HPP__
 
@@ -24,3 +25,5 @@
 SoundSource* create_dummy_sound_source();
 
 #endif
+
+// EOF
